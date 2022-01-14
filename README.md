e81f809c584658364cf2451b57b473f232505579
Fabian <Fabian.Heudorfer@posteo.de>
2022-01-14 15:10:23 +0100

    Final Version for Task 3

--- README.md
+++ README.md
@@ -1,25 +1,82 @@
-# Covid 19 vaccination data.
+# Covid-19 Data - A Data Science Project
 This project is part of the course Applied Data Science with Python
 
 
-Name: Fabian Heudorfer 
-Course: Applied Data Science with Python
+**Name:** Fabian Heudorfer   
+
+**Course:** Applied Data Science with Python
 
 
-Project description:
-Based on the Covid19 dataset from ‘Our World In Data’ (https://ourworldindata.org) I want to visualize the effects on all countries in the world. 
+## Project Description:
 
-The dataset has 44 columns and approx. 2 years of data. It starts from 2020-01-30 until 2021-11-13. I will compare this data to the data acquired in Germany. The vaccination data for Germany will be received from the Robert-Koch-Institut and the Department of Health.
+Based on the Covid-19 dataset from ‘Our World In Data’ https://ourworldindata.org I want to visualize the effects on all countries in the world. 
 
-Our World in Data:
+
+## Our World in Data:
+
 https://github.com/owid/covid-19-data/ 
+
 https://github.com/owid/covid-19-data/blob/master/public/data/README.md
 
+## All Available Colums in the Dataset:   
+['iso_code', 'continent', 'location', 'date', 'total_cases', 'new_cases',
+'new_cases_smoothed', 'total_deaths', 'new_deaths',
+       'new_deaths_smoothed', 'total_cases_per_million',
+       'new_cases_per_million', 'new_cases_smoothed_per_million',
+       'total_deaths_per_million', 'new_deaths_per_million',
+       'new_deaths_smoothed_per_million', 'reproduction_rate', 'icu_patients',
+       'icu_patients_per_million', 'hosp_patients',
+       'hosp_patients_per_million', 'weekly_icu_admissions',
+       'weekly_icu_admissions_per_million', 'weekly_hosp_admissions',
+       'weekly_hosp_admissions_per_million', 'new_tests', 'total_tests',
+       'total_tests_per_thousand', 'new_tests_per_thousand',
+       'new_tests_smoothed', 'new_tests_smoothed_per_thousand',
+       'positive_rate', 'tests_per_case', 'tests_units', 'total_vaccinations',
+       'people_vaccinated', 'people_fully_vaccinated', 'total_boosters',
+       'new_vaccinations', 'new_vaccinations_smoothed',
+       'total_vaccinations_per_hundred', 'people_vaccinated_per_hundred',
+       'people_fully_vaccinated_per_hundred', 'total_boosters_per_hundred',
+       'new_vaccinations_smoothed_per_million',
+       'new_people_vaccinated_smoothed',
+       'new_people_vaccinated_smoothed_per_hundred', 'stringency_index',
+       'population', 'population_density', 'median_age', 'aged_65_older',
+       'aged_70_older', 'gdp_per_capita', 'extreme_poverty',
+       'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers',
+       'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand',
+       'life_expectancy', 'human_development_index',
+       'excess_mortality_cumulative_absolute', 'excess_mortality_cumulative',
+       'excess_mortality', 'excess_mortality_cumulative_per_million']
+
+## Preselected Columns to Reduce Complexity:  
+['total_cases_per_million', 'new_cases_smoothed_per_million',
+'total_deaths_per_million', 'total_vaccinations_per_hundred',
+'people_fully_vaccinated_per_hundred','icu_patients_per_million',
+]
 
 
-Files:
-
-Goal: 
-Compare the vaccination progress of the countries worldwide. Investigate and correlate the vaccination progress to new cases and deaths per million inhabitants. The results should be presented as a python dashboard.
 
 
+## Goal:
+
+Visuallize and compare the Covid-19 effects on the countries worldwide. 
+The results should be presented as a python dashboard.
+
+
+## How To Use The Code:
+
+1. Clone the gitHub repository:  
+    ``` >> git clone https://github.com/FabianHeu/DataScienceProject.git ```
+3. run the python script main.py  
+    ``` >> python main.py```
+
+
+## Sources:
+
+The App Design is based on:  
+
+https://dash.gallery/dash-uber-rides-demo/
+
+##Jupyter Notebook:
+
+To test single functions and play around with the code there is jupyter notbook called main.ipynb in the jupyter subfolder.   
+```>> jupyter notebook main.ipynb```
--- main.py
+++ main.py
@@ -3,27 +3,34 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
-from plotly import graph_objs as go
 import pandas as pd
-import numpy as np
-from datetime import datetime
+from datetime import date, timedelta
 
 
 # Initialize app -------------------------------------------------------------
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}], )
-app.title = "Covid 19 vaccination data"
+app.title = "Covid 19 data"
 server = app.server
 
-# ######################## Load my own data ###################################################################
+# ######################## Load  data ##########################################################################
 # data from 'our world in Data'
 dataframeGlobal = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
-columnsList = dataframeGlobal.columns
-dataframeFiltered = dataframeGlobal[dataframeGlobal.date.eq('2021-12-16')]
-dfNZ = dataframeGlobal[dataframeGlobal['location'] == 'New Zealand']
 
-# #############################################################################################################
+# select data which wants to be shown
+columnsList = ['total_cases_per_million', 'new_cases_smoothed_per_million',
+              'total_deaths_per_million', 'total_vaccinations_per_hundred',
+              'people_fully_vaccinated_per_hundred','icu_patients_per_million',
+              ]
 
-# Layout of Dash App
+# Indroduction for User in web app
+explanation = "Select a location with the dropdown. More than one country can be selected and the data can be compared.\n" \
+              "The data can be selected with the data dropdown. Here only one selection can be made. The data will always be shown until" \
+              " yesterday which is the most up to date data. "
+
+# get the actual date
+today = str(date.today() - timedelta(1))
+
+# Layout of Dash App the css files are placed in ../assets
 app.layout = html.Div(
     children=[
         html.Div(
@@ -31,7 +38,7 @@ app.layout = html.Div(
             children=[
                 # Column for user controls
                 html.Div(
-                    className="four columns div-user-controls",
+                    className="four columns user-controls",
                     children=[
                         html.A(
                             html.Img(
@@ -40,24 +47,9 @@ app.layout = html.Div(
                             ),
                             href="https://www.oth-regensburg.de",
                         ),
-                        html.H2("Covid -19 Dashboard"),
+                        html.H2("Covid - 19 Dashboard"),
                         html.P(
-                            """Select different days using the date picker or by selecting
-                            different time frames on the histogram."""
-                        ),
-                        html.Div(
-                            className="div-for-dropdown",
-                            children=[
-                                dcc.DatePickerSingle(
-                                    id="date-picker",
-                                    min_date_allowed=datetime(2020, 1, 1),
-                                    max_date_allowed=datetime(2021, 9, 30),
-                                    initial_visible_month=datetime(2020, 1, 1),
-                                    date=datetime(2020, 1, 1).date(),
-                                    display_format="MMMM D, YYYY",
-                                    style={"border": "0px solid black"},
-                                )
-                            ],
+                            explanation
                         ),
                         # Change to side-by-side for mobile layout
                         html.Div(
@@ -71,8 +63,8 @@ app.layout = html.Div(
                                             id="location-dropdown",
                                             options=[{'label': i, 'value': i} for i in dataframeGlobal['location'].unique()],
                                             placeholder="Select a location",
-                                            value='Germany'
-
+                                            value=['Germany'],
+                                            multi=True,
                                         )
                                     ],
                                 ),
@@ -91,9 +83,6 @@ app.layout = html.Div(
                                 ),
                             ],
                         ),
-                        html.P(id="total-rides"),
-                        html.P(id="total-rides-selection"),
-                        html.P(id="date-value"),
                         dcc.Markdown(
                             """
                             Source: [Github](https://github.com/FabianHeu/DataScienceProject)
@@ -103,7 +92,7 @@ app.layout = html.Div(
                 ),
                 # Column for app graphs and plots
                 html.Div(
-                    className="eight columns div-for-charts bg-grey",
+                    className="eight columns worldmap and graph",
                     children=[
                         dcc.Graph(id="worldmap"),
                         dcc.Graph(id="graph"),
@@ -114,28 +103,30 @@ app.layout = html.Div(
     ]
 )
 
+# call back to update the worldmap
 @app.callback(
     Output(component_id="worldmap", component_property="figure"),
-
     Input(component_id="location-dropdown", component_property="value"),
     Input(component_id="data-dropdown", component_property="value"),
 
 )
 def update_worldmap(location, data):
-    dataframeFiltered = dataframeGlobal[dataframeGlobal.date.eq('2021-12-16')]
+    dataframeFiltered = dataframeGlobal[dataframeGlobal.date.eq(today)]
     worldmap = px.choropleth(
         data_frame=dataframeFiltered,
         locations='iso_code',
         hover_name='location',
         color=data,
         color_continuous_scale=px.colors.sequential.Viridis,
+
     )
     worldmap.update_layout(
         showlegend=False,
         margin=dict(t=0, l=0, b=0, r=0),
         geo=dict(bgcolor='rgba(0,0,0,0)'),
         paper_bgcolor='rgba(0,0,0,0)',
-        coloraxis_showscale=False
+        coloraxis_showscale=False,
+        dragmode=False,
     )
     worldmap.update_geos(
         showlakes=False,
@@ -145,24 +136,27 @@ def update_worldmap(location, data):
     return worldmap
 
 
+# call back to update the graph
 @app.callback(
     Output(component_id="graph", component_property="figure"),
     Input(component_id="data-dropdown", component_property="value"),
     Input(component_id="location-dropdown", component_property="value")
 )
-def update_selection(data, country):
-    graph = px.area(
-        data_frame=dataframeGlobal[dataframeGlobal['location'] == country],
+def update_graph(dataSelection, country):
+    graph = px.line(
+        data_frame=dataframeGlobal.loc[dataframeGlobal['location'].isin(list(country))],
         x='date',
-        y=data,
+        y=dataSelection,
+        color="location",
+        line_group='location',
     )
     graph.update_layout(
-        showlegend=False,
         plot_bgcolor='rgba(0,0,0,0)',
         margin=dict(t=0, l=0, b=0, r=0),
         paper_bgcolor='rgba(0,0,0,0)',
         xaxis=dict(
             showline=False,
+            zeroline=False,
             showgrid=False,
             title='',
             tickfont=dict(size=15, family='verdana', color='rgba(0,162,137,100)'),
@@ -171,8 +165,9 @@ def update_selection(data, country):
             showgrid=False,
             zeroline=False,
             showline=False,
-            showticklabels=False,
-            title='',
+            showticklabels=True,
+            titlefont=dict(size=15, family='verdana', color='rgba(0,162,137,100)'),
+            tickfont=dict(size=15, family='verdana', color='rgba(0,162,137,100)'),
         ),
     )
     return graph
